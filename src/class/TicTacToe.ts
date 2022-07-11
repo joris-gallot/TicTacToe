5ff2b8e82e549fc63904a4eef6c403e2a39e1a0f
Joris <you@example.com>
2022-07-12 00:48:47 +0200

    feat: play a complete game

--- src/class/TicTacToe.ts
+++ src/class/TicTacToe.ts
@@ -1,6 +1,7 @@
-import { SquareValue } from "../enum/SquareValue";
+import { SquareValue } from "./../enum/SquareValue";
 
 export class TicTacToe {
+  public winner: string | undefined;
   public playerTurn: 1 | 2 = 1;
   private grid: SquareValue[][] = [];
 
@@ -34,5 +35,74 @@ export class TicTacToe {
       this.grid[x][y] = SquareValue.O;
       this.playerTurn = 1;
     }
+
+    this.winner = this.detectWinner();
+  }
+
+  private detectWinner(): string | undefined {
+    const winSquareValue =
+      this.detectHorizontalWin() ||
+      this.detectVerticalWin() ||
+      this.detectDiagonalWin();
+
+    if (winSquareValue) {
+      return winSquareValue === SquareValue.X ? this.p1 : this.p2;
+    }
+
+    return undefined;
+  }
+
+  public isDraw(): boolean {
+    return this.winner === undefined && this.isFull();
+  }
+
+  private isFull(): boolean {
+    return this.grid.every((row) =>
+      row.every((square) => square !== SquareValue.EMPTY)
+    );
+  }
+
+  private detectHorizontalWin(): SquareValue | undefined {
+    for (let i = 0; i < this.grid.length; i++) {
+      if (
+        this.grid[i][0] === this.grid[i][1] &&
+        this.grid[i][1] === this.grid[i][2]
+      ) {
+        return this.grid[i][0];
+      }
+    }
+
+    return undefined;
+  }
+
+  private detectVerticalWin(): SquareValue | undefined {
+    for (let i = 0; i < this.grid.length; i++) {
+      if (
+        this.grid[0][i] === this.grid[1][i] &&
+        this.grid[1][i] === this.grid[2][i]
+      ) {
+        return this.grid[0][i];
+      }
+    }
+
+    return undefined;
+  }
+
+  private detectDiagonalWin(): SquareValue | undefined {
+    if (
+      this.grid[0][0] === this.grid[1][1] &&
+      this.grid[1][1] === this.grid[2][2]
+    ) {
+      return this.grid[0][0];
+    }
+
+    if (
+      this.grid[0][2] === this.grid[1][1] &&
+      this.grid[1][1] === this.grid[2][0]
+    ) {
+      return this.grid[0][2];
+    }
+
+    return undefined;
   }
 }
--- tests/game.test.ts
+++ tests/game.test.ts
@@ -39,11 +39,66 @@ describe("creating game", () => {
 
     game.play(0, 0);
     expect(game.playerTurn).toBe(2);
+    expect(game.getValue(0, 0)).toBe(SquareValue.X);
 
     expect(() => game.play(0, 0)).toThrowError("Square already taken");
     expect(game.playerTurn).toBe(2);
 
     game.play(0, 1);
     expect(game.playerTurn).toBe(1);
+    expect(game.getValue(0, 1)).toBe(SquareValue.O);
+  });
+
+  it("should win horizontally", () => {
+    const game = new TicTacToe("foo", "bar");
+
+    game.play(0, 0); // P1
+    game.play(1, 0); // P2
+    game.play(0, 1); // P1
+    game.play(1, 1); // P2
+    game.play(0, 2); // P1
+
+    expect(game.winner).toBe("foo"); // P1 win
+  });
+
+  it("should win vertically", () => {
+    const game = new TicTacToe("foo", "bar");
+
+    game.play(0, 0); // P1
+    game.play(1, 1); // P2
+    game.play(1, 0); // P1
+    game.play(2, 1); // P2
+    game.play(2, 0); // P1
+
+    expect(game.winner).toBe("foo"); // P1 win
+  });
+
+  it("should win diagonally", () => {
+    const game = new TicTacToe("foo", "bar");
+
+    game.play(0, 0); // P1
+    game.play(1, 0); // P2
+    game.play(1, 1); // P1
+    game.play(1, 2); // P2
+    game.play(2, 2); // P1
+
+    expect(game.winner).toBe("foo"); // P1 win
+  });
+
+  it("should draw", () => {
+    const game = new TicTacToe("foo", "bar");
+
+    game.play(0, 0); // P1
+    game.play(0, 1); // P2
+    game.play(0, 2); // P1
+    game.play(1, 0); // P2
+    game.play(1, 2); // P1
+    game.play(1, 1); // P2
+    game.play(2, 0); // P1
+    game.play(2, 2); // P2
+    game.play(2, 1); // P1
+
+    expect(game.winner).toBeUndefined(); // Draw
+    expect(game.isDraw()).toBe(true);
   });
 });
